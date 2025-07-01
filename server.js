6e07509c532db78813418dd3b4dd720b765634d4
Harsh raj <140217747+Rajharsh36@users.noreply.github.com>
2025-07-01 22:56:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,21 +7,42 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-// Serve index.html from root
+// In-memory message store with timestamps
+let messages = [];
+
+// Prune messages older than 20 minutes
+function pruneOldMessages() {
+    const now = Date.now();
+    const twentyMinutes = 20 * 60 * 1000;
+    messages = messages.filter(msg => now - msg.time <= twentyMinutes);
+}
+
+// Serve the HTML file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Socket.io logic
 io.on('connection', (socket) => {
-    console.log('A user connected');
+    console.log(`User connected: ${socket.id}`);
 
-    socket.on('chat message', (msg) => {
+    // Send the last 20 mins of messages
+    pruneOldMessages();
+    socket.emit('chat history', messages);
+
+    socket.on('chat message', (text) => {
+        const msg = {
+            id: socket.id,
+            text,
+            time: Date.now()
+        };
+        messages.push(msg);
+        pruneOldMessages();
         io.emit('chat message', msg);
     });
 
     socket.on('disconnect', () => {
-        console.log('A user disconnected');
+        console.log(`User disconnected: ${socket.id}`);
     });
 });
 
